8920128f185c1fab9bd33c41fa5442a8d2936822
Mauricio Hernandez <mau.orellana9191@gmail.com>
2022-05-15 13:00:40 -0600

    Proyecto reportes c# entity framwork

---
+++ Reportes/Controllers/ProductoController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+using Reportes.Rpts;
+
+
+namespace Reportes.Controllers
+{
+    public class ProductoController : Controller
+    {
+
+        public ActionResult ReporteSimple()
+        {
+            return View();
+        }
+
+
+        public ActionResult ReporteParametros()
+        {
+            return View();
+        }
+
+        public ActionResult VerReporte()
+        {
+            var reporte = new ReportClass();
+            reporte.FileName = Server.MapPath("/Rpts/RptSimple.rpt");
+
+            //Conexion para el reporte
+            var coninfo = ReporteConexion.GetConnection();
+            TableLogOnInfo logoninfo = new TableLogOnInfo();
+            Tables tables;
+            tables = reporte.Database.Tables;
+            foreach (Table item in tables)
+            {
+                logoninfo = item.LogOnInfo;
+                logoninfo.ConnectionInfo = coninfo;
+                item.ApplyLogOnInfo(logoninfo);
+            }
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            Stream stream = reporte.ExportToStream(ExportFormatType.PortableDocFormat);
+            return new FileStreamResult(stream, "application/pdf");
+
+
+        }
+
+
+        public ActionResult VerReporteParam(String parametro)
+        {
+            var reporte = new ReportClass();
+            reporte.FileName = Server.MapPath("/Rpts/ReporteParametro.rpt");
+
+            reporte.SetParameterValue("paramCategorias", parametro);
+
+            //Conexion para el reporte
+            var coninfo = ReporteConexion.GetConnection();
+            TableLogOnInfo logoninfo = new TableLogOnInfo();
+            Tables tables;
+            tables = reporte.Database.Tables;
+            foreach (Table item in tables)
+            {
+                logoninfo = item.LogOnInfo;
+                logoninfo.ConnectionInfo = coninfo;
+                item.ApplyLogOnInfo(logoninfo);
+            }
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            Stream stream = reporte.ExportToStream(ExportFormatType.PortableDocFormat);
+            return new FileStreamResult(stream, "application/pdf");
+
+
+        }
+
+
+
+
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Producto/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Producto/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Reportes/Rpts/ReporteConexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reportes.Rpts
+{
+    public class ReporteConexion
+    {
+       public static CrystalDecisions.Shared.ConnectionInfo GetConnection()
+        {
+            CrystalDecisions.Shared.ConnectionInfo infocon = new CrystalDecisions.Shared.ConnectionInfo();
+
+            infocon.ServerName = @"DESKTOP-7IIODCS\SQLEXPRESS";
+            infocon.DatabaseName = "RESTAURANTE";
+            infocon.IntegratedSecurity = true;
+
+            return infocon;
+        }
+    }
+}
